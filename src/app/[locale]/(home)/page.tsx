cbab07f5b5a8154b5e0b713397c6e8ce6c25d432
hidekazu <hidekazu@craft.do>
2025-07-19 16:28:30 +0200

    complate large card

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -11,9 +11,57 @@ import React from "react";
 import LargeCard from "../../../components/cards/LargeCard";
 import Hero from "./Hero";
 
+import LargeCardImage1 from "../../../../public/images/home/image_large_card_1.jpg";
+import LargeCardImage2 from "../../../../public/images/home/image_large_card_2.jpg";
+import IconBlankDoc from "@/components/icons/Blank Doc";
+import IconScribble from "@/components/icons/Scribble";
+import IconAi from "@/components/icons/AI";
+import IconCloud from "@/components/icons/Cloud";
+import IconTodo from "@/components/icons/Todo";
+import IconClock from "@/components/icons/Clock";
+import IconDoc from "@/components/icons/Doc";
+import IconCalendar from "@/components/icons/Calendar";
+
 export const metadata = {
   title: "Craft - Docs and Notes Editor",
 };
+
+const writingFeatures = [
+  {
+    icon: IconBlankDoc,
+    label: "Templates",
+  },
+  {
+    icon: IconScribble,
+    label: "Whiteboards",
+  },
+  {
+    icon: IconAi,
+    label: "Write with AI",
+  },
+  {
+    icon: IconCloud,
+    label: "Publish & share",
+  },
+];
+const planningFeatures = [
+  {
+    icon: IconTodo,
+    label: "Tasks",
+  },
+  {
+    icon: IconClock,
+    label: "Reminders",
+  },
+  {
+    icon: IconDoc,
+    label: "Daily Notes",
+  },
+  {
+    icon: IconCalendar,
+    label: "Calender",
+  },
+];
 
 export default function Home() {
   return (
@@ -21,11 +69,26 @@ export default function Home() {
       <Hero />
       <div className="max-w-[1400px] mx-auto">
         <Icons />
-        <LargeCard />
+        <LargeCard
+          image={LargeCardImage1}
+          label="writing"
+          title="From first thought to final form"
+          description="The right tool unlocks your flow. Craft helps you shape ideas into something rich, structured, and uniquely yours."
+          features={writingFeatures}
+          bgColor="bg-purple-2"
+        />
         <Overview />
         <SmallCards />
         <section>{/* quote */}</section>
-        {/* <LargeCard /> */}
+        <LargeCard
+          image={LargeCardImage2}
+          label="planning"
+          title="Planning that doesn’t feel like work"
+          description="From quick to-dos to long-term goals, Craft helps you plan without friction and stay focused without forcing it."
+          features={planningFeatures}
+          bgColor="bg-yellow-2"
+          reverse
+        />
         <EditorsChoice />
         <Customization />
         <WhatOthersAreSaying />
--- src/components/cards/LargeCard.tsx
+++ src/components/cards/LargeCard.tsx
@@ -1,36 +1,46 @@
-import Image from "next/image";
-import React from "react";
-import LargeCardImage1 from "../../../public/images/home/image_large_card_1.jpg";
+import Image, { StaticImageData } from "next/image";
+import React, { ReactElement } from "react";
 import PaperTexture from "../../../public/images/common/image_paper_texture.png";
 import GlassButton from "../buttons/GlassButton";
 import IconScribble from "../icons/Scribble";
 import IconAi from "../icons/AI";
 import IconCloud from "../icons/Cloud";
 import IconBlankDoc from "../icons/Blank Doc";
+import { cn } from "@/utils/ClassNames";
 
-const features = [
-  {
-    icon: IconBlankDoc,
-    label: "Templates",
-  },
-  {
-    icon: IconScribble,
-    label: "Whiteboards",
-  },
-  {
-    icon: IconAi,
-    label: "Write with AI",
-  },
-  {
-    icon: IconCloud,
-    label: "Publish & share",
-  },
-];
+type Feature = {
+  icon: ({
+    className,
+  }: {
+    className?: string | undefined;
+  }) => React.JSX.Element;
+  label: string;
+};
 
-export default function LargeCard() {
+type LargeCardProps = {
+  image: StaticImageData;
+  bgColor: string;
+  label: string;
+  title: string;
+  description: string;
+  features: Feature[];
+  reverse?: boolean;
+};
+
+export default function LargeCard({
+  image,
+  bgColor,
+  label,
+  title,
+  description,
+  features,
+  reverse = false,
+}: LargeCardProps) {
   return (
-    <section className="relative p-4 rounded-3xl space-y-6 shadow-block">
-      <div className="absolute bg-purple-2 inset-0 -z-10 rounded-3xl h-full" />
+    <section className="relative p-4 md:p-8 lg:p-0 rounded-3xl space-y-6 shadow-block">
+      <div
+        className={cn("absolute  inset-0 -z-10 rounded-3xl h-full", bgColor)}
+      />
       <Image
         src={PaperTexture}
         alt="large card image1"
@@ -38,23 +48,25 @@ export default function LargeCard() {
         fill
       />
 
-      <div className="flex flex-col gap-6 lg:grid lg:grid-cols-2 lg:gap-10 lg:p-8 z-20">
+      <div
+        className={cn(
+          "flex flex-col gap-6 lg:flex-row  lg:gap-10 lg:p-8 z-20",
+          reverse && "flex-col-reverse lg:flex-row-reverse"
+        )}
+      >
         <Image
-          src={LargeCardImage1}
+          src={image}
           alt="large card image1"
-          className="rounded-2xl aspect-square sm:aspect-4/3 lg:aspect-auto object-cover lg:h-[736px] lg:mb-0"
+          className="rounded-2xl aspect-square sm:aspect-4/3 lg:aspect-auto object-cover lg:h-[736px] lg:w-1/2 lg:mb-0"
         />
         <div className="py-6 px-4 space-y-8 lg:flex lg:flex-col lg:justify-between lg:items-start">
           <div className="space-y-8 sm:flex  lg:flex-col lg:h-full lg:justify-between sm:gap-12">
             <div className="space-y-8 sm:flex-1 lg:flex-none">
-              <h4 className="text-black-600">writing</h4>
-              <h2>From first thought to final form</h2>
-              <p className="text-black-600">
-                The right tool unlocks your flow. Craft helps you shape ideas
-                into something rich, structured, and uniquely yours.
-              </p>
+              <h4 className="text-black-600">{label}</h4>
+              <h2>{title}</h2>
+              <p className="text-black-600">{description}</p>
             </div>
-            <div className="grid gap-4 lg:grid-cols-2 sm:w-[280px] lg:w-full">
+            <div className="grid gap-4 lg:grid-cols-2 sm:w-[280px] lg:w-full md:gap-8">
               {features.map((feature) => (
                 <div key={feature.label} className="flex items-center gap-4">
                   <feature.icon className="h-[40px] w-[40px]" />
